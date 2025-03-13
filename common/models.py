b0b580b49024fa3dce929385ae4f57d08de6d9cd
chungoid <flip.m.lacombe@gmail.com>
2025-03-12 21:35:12 -0500

    submenu updates

--- common/models.py
+++ common/models.py
@@ -28,16 +28,16 @@ class InterfaceData:
 
 @dataclass
 class ScanData:
-    tool: str
-    scan_profile: str
-    preset_description = str
-    window_name: str
-    pane_id: str
-    internal_name: str  # Added internal name for UI display purposes.
-    interface: str
-    lock_status: bool
-    cmd_str: str = ""
-    timestamp: float = None
+    tool: str # tool that sent the scan
+    scan_profile: str # entire scan profile dict
+    window_name: str # tmux name for window the scan resides in
+    pane_id: str # tmux pane id
+    internal_name: str  # internal name for UI manager to use in menus
+    interface: str # scan interface used
+    lock_status: bool # interface lock status, currently not in use. would be in /var/lockfile
+    cmd_str: str = "" # the scans cmd_dict (in string form) that was run
+    preset_description: str = "" # specifically the description key from the scan profile
+    timestamp: float = None # when the scan was started
 
     def to_dict(self):
         return asdict(self)
